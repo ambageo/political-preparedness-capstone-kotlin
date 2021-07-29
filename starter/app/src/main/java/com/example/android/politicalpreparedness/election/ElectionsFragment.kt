177d8b394438e6a6ba80b33ca890fcac71f4065b
GeorgeAmpartzidis <george.ampartzidis@mpway.com>
2021-07-29 11:29:04 +0100

    Initial commit - added layout components in fragment_launch.xml

--- starter/app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.android.politicalpreparedness.databinding.FragmentElectionBinding
 
 class ElectionsFragment: Fragment() {
 
@@ -17,13 +18,15 @@ class ElectionsFragment: Fragment() {
         //TODO: Add ViewModel values and create ViewModel
 
         //TODO: Add binding values
-
+        val binding = FragmentElectionBinding.inflate(inflater)
         //TODO: Link elections to voter info
 
         //TODO: Initiate recycler adapters
 
         //TODO: Populate recycler adapters
 
+
+        return binding.root
     }
 
     //TODO: Refresh adapters when fragment loads
