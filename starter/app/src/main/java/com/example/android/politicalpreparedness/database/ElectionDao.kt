c33167eff3fbef1eecd6ebbc9fb010c9aaa15ccc
GeorgeAmpartzidis <george.ampartzidis@mpway.com>
2021-07-31 10:52:26 +0100

    Added DAO methods for saved elections

--- starter/app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
@@ -3,17 +3,26 @@ package com.example.android.politicalpreparedness.database
 import androidx.lifecycle.LiveData
 import androidx.room.*
 import com.example.android.politicalpreparedness.network.models.Election
+import com.example.android.politicalpreparedness.network.models.SavedElection
 
 @Dao
 interface ElectionDao {
 
     //DONE: Add insert query
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertElection(election: Election)
+    suspend fun insertAllElections(vararg elections: Election)
 
     //DONE: Add select all election query
     @Query("select * from election_table")
-    suspend fun getElections(): LiveData<List<Election>>
+    suspend fun getAllElections(): LiveData<List<Election>>
+
+    //DONE: Add clear query
+    @Query("delete from election_table")
+    suspend fun deleteAll()
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveElection(id: SavedElection)
+
 
     //DONE: Add select single election query
     @Query("select * from election_table where id = :id")
@@ -23,7 +32,5 @@ interface ElectionDao {
     @Query("delete from election_table where id = :id")
     suspend fun deleteElectionById(id: Int)
 
-    //DONE: Add clear query
-    @Query("delete from election_table")
-    suspend fun deleteAll()
+
 }
--- starter/app/src/main/java/com/example/android/politicalpreparedness/network/models/Election.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/network/models/Election.kt
@@ -7,9 +7,17 @@ import java.util.*
 @Entity(tableName = "election_table")
 data class Election(
         @PrimaryKey val id: Int,
-        @ColumnInfo(name = "name")val name: String,
-        @ColumnInfo(name = "electionDay")val electionDay: Date,
-        @Embedded(prefix = "division_") @Json(name="ocdDivisionId") val division: Division
+        @ColumnInfo(name = "name") val name: String,
+        @ColumnInfo(name = "electionDay") val electionDay: Date,
+        @Embedded(prefix = "division_") @Json(name = "ocdDivisionId") val division: Division
+)
+
+/*
+* Since all data we need for an Election is already stored in election_table, we can just save the
+* id of an Election we want to follow */
+@Entity(tableName = "saved_election_table")
+data class SavedElection(
+        @PrimaryKey val id: Int
 )
 
 // TODO: Add extension function to convert from Database Object to Domain Object
