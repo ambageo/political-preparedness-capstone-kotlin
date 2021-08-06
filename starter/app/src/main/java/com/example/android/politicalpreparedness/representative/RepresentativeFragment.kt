fe0c63f5c1bfabb53d59060e034f81a1ce8287e0
GeorgeAmpartzidis <george.ampartzidis@mpway.com>
2021-08-06 11:43:05 +0100

    Fixed error with view!! in hideKeyboard

--- starter/app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeFragment.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeFragment.kt
@@ -128,7 +128,7 @@ class RepresentativeFragment : Fragment() {
 
     private fun hideKeyboard() {
         val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view!!.windowToken, 0)
+        imm.hideSoftInputFromWindow(requireView().windowToken, 0)
     }
 
 }
